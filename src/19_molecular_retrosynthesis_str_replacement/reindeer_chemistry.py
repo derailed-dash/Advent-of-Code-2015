8e4272567511598a5e8258284ca63d836a08bac2
Dazbo <dazbo@just2good.co.uk>
2021-03-14 17:12:55 +0000

    Day 19 Part 2

--- src/19_molecular_retrosynthesis_str_replacement/reindeer_chemistry.py
+++ src/19_molecular_retrosynthesis_str_replacement/reindeer_chemistry.py
@@ -30,7 +30,15 @@ Part 1:
 Part 2:
     Number of iterations to go from e to target molecule.
 
-    This time, go through all target groups.
+    E.g. HOHOHO -> (3) HHH -> (1) OH -> (1) H -> (1) e
+
+    Start with the target medicine molecule.
+    Go through all target groups, and substitute with src molecule.
+    Ingore e substitutions for now.
+    Count how many substitions are possible with this target, and store in a list.
+    Update the current molecule.  Repeat until the molecule is no longer updated.
+
+    Once no more updates are possible, perform substition to get back to e.
 """
 from __future__ import absolute_import
 import os
@@ -44,8 +52,8 @@ INPUT_FILE = "input/input.txt"
 SAMPLE_INPUT_FILE = "input/sample_input.txt"
 
 def main():
-    input_file = os.path.join(SCRIPT_DIR, SAMPLE_INPUT_FILE)
-    # input_file = os.path.join(SCRIPT_DIR, INPUT_FILE)
+    # input_file = os.path.join(SCRIPT_DIR, SAMPLE_INPUT_FILE)
+    input_file = os.path.join(SCRIPT_DIR, INPUT_FILE)
     with open(input_file, mode="rt") as f:
         data = f.read().splitlines()
 
@@ -54,23 +62,69 @@ def main():
     new_molecules = substitute_groups(src_groups, medicine_molecule)
 
     unique_new_molecules = set(new_molecules)
-    print(unique_new_molecules)
-    print(f"Part 1: Identified {len(unique_new_molecules)} unique molecules")
+    # print(unique_new_molecules)
+    print(f"Part 1: Identified {len(unique_new_molecules)} unique molecules.")
+
+    synthesis_stack = retrosynthesis(target_groups, medicine_molecule)
+    synthesis_steps = sum(step[0] for step in synthesis_stack)
+    print(f"Part 2: Synthesis stack requires {synthesis_steps} steps.")
+    # print(synthesis_stack)
 
 
-def substitute_groups(groups, medicine_molecule) -> list:
+def retrosynthesis(target_groups: dict, target_molecule: str) -> list:
+    synthesis_stack = []
+    current_molecule = target_molecule
+
+    # start by doing all the substitions, without e
+    # repeat until the molecule is not modified
+    while True:
+        molecule_modified = False
+
+        for tgt_grp, src_grp in target_groups.items():
+            if src_grp == 'e':
+                continue
+
+            # count how many matches of target first
+            substitutions = current_molecule.count(tgt_grp)
+
+            # then replace them all
+            if substitutions > 0:
+                current_molecule = current_molecule.replace(tgt_grp, src_grp)
+                molecule_modified = True
+                synthesis_stack.append([substitutions, current_molecule])
+        
+        if not molecule_modified:
+            break
+
+    # now replace target with e
+    for tgt_grp, src_grp in target_groups.items():
+        if src_grp != 'e':
+            continue
+
+        # count how many matches of target first
+        substitutions = current_molecule.count(tgt_grp)
+
+        # then replace them all
+        if substitutions > 0:
+            current_molecule = current_molecule.replace(tgt_grp, src_grp)
+            synthesis_stack.append([substitutions, current_molecule])
+
+    return synthesis_stack
+
+
+def substitute_groups(groups: dict, molecule: str) -> list:
     new_molecules = []
 
     # go through all the groups we have substitutions for
     for group, targets in groups.items():
         # get all matching positions for this group
-        group_matches = re.finditer(group, medicine_molecule)
+        group_matches = re.finditer(group, molecule)
 
         # move left to right, matching group one at a time
         for group_match in group_matches:
             start, end = group_match.span()
-            prefix = medicine_molecule[:start]
-            suffix = medicine_molecule[end:]
+            prefix = molecule[:start]
+            suffix = molecule[end:]
 
             # replace the current group occurrence with each target
             for target in targets:

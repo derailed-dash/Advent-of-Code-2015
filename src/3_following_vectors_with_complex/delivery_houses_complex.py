562a77dda01d7ded06584bf91ec2d76b1057cb7f
Dazbo <derailed.dash@gmail.com>
2021-06-27 10:31:27 +0100

    Refactored day 3 to use complex numbers

--- src/3_following_vectors_with_complex/delivery_houses_complex.py
+++ src/3_following_vectors_with_complex/delivery_houses_complex.py
@@ -4,14 +4,21 @@ Date: 13/01/2021
 
 Solving https://adventofcode.com/2015/day/3
 
+Solution 2 of 2:
+    Same as solution 1, but let's use complex numbers to store the x, y coordinates, rather than lists/tuples.
+    That way, we only have to store one thing for each move.
+    This removes a load of code duplicaton, and over half the time!
+
 Part 1:
     Given input of directions in >^<v format, count how many locations were visited.
+    Each location is represented using a complex number.
+    Store each visited location in a set.
 
 Part 2:
     Santa and Robosanta alternate to follow the directions given.
     Count how many locations were visited by either Santa or Robosanta.
+    Here, we use %2 to send alternate directions to each of Santa and Robosanta.    
 """
-import sys
 import os
 import time
 
@@ -20,10 +27,10 @@ INPUT_FILE = "input/input.txt"
 SAMPLE_INPUT_FILE = "input/sample_input.txt"
 
 VECTORS = {
-    '^': [0, 1],
-    '>': [1, 0],
-    'v': [0, -1],
-    '<': [-1, 0]
+    '^': 0+1j,
+    '>': 1+0j,
+    'v': 0-1j,
+    '<': -1+0j
 }
 
 def main():
@@ -32,34 +39,30 @@ def main():
     with open(input_file, mode="rt") as f:
         data = f.read()
 
-    current_x = current_y = 0
+    current_location = 0+0j
     visited_locations = set()
-    visited_locations.add(tuple([current_x, current_y]))
+    visited_locations.add(current_location)
 
     for vector in data:
-        current_x += VECTORS[vector][0]
-        current_y += VECTORS[vector][1]
-        visited_locations.add(tuple([current_x, current_y]))
+        current_location += VECTORS[vector]
+        visited_locations.add(current_location)
 
     print(f"Santa visited {len(visited_locations)} locations.")
 
-    santa_x = santa_y = 0
-    robosanta_x = robosanta_y = 0
+    santa_location = robosanta_location = 0+0j
 
     santa_visited_locations = set()
-    santa_visited_locations.add(tuple([santa_x, santa_y]))
+    santa_visited_locations.add(santa_location)
     robosanta_visited_locations = set()
-    robosanta_visited_locations.add(tuple([robosanta_x, robosanta_y]))
+    robosanta_visited_locations.add(robosanta_location)
 
     for i, vector in enumerate(data):
         if i % 2 == 1:
-            santa_x += VECTORS[vector][0]
-            santa_y += VECTORS[vector][1]
-            santa_visited_locations.add(tuple([santa_x, santa_y]))
+            santa_location += VECTORS[vector]
+            santa_visited_locations.add(santa_location)
         else:
-            robosanta_x += VECTORS[vector][0]
-            robosanta_y += VECTORS[vector][1]
-            robosanta_visited_locations.add(tuple([robosanta_x, robosanta_y]))
+            robosanta_location += VECTORS[vector]
+            robosanta_visited_locations.add(robosanta_location)
 
     visited_locations = santa_visited_locations.union(robosanta_visited_locations)
     print(f"Santa and Robosanta visited {len(visited_locations)} locations.")
